0d15199880428144e995034c88966e307c7607b3
Steven Kalt <kalt.steven@gmail.com>
2022-12-27 00:04:14 -0500

    chore: deploy

---
+++ _app/immutable/chunks/2-7959ac86.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-efac176d.js";export{t as component};
--- _app/immutable/chunks/2-9a5b76e6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-14e64d74.js";export{t as component};
